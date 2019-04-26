fc7658c6a14839eb1e1d5885baaa4a4f50151a54
Maks <maxim.labutin1994@gmail.com>
2019-04-26 17:40:13 +0300

    Add Diagonal Traverse task

---
+++ DiagonalTraverse/DiagonalTraverse/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DiagonalTraverse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiagonalTraverse")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("24362926-7c5f-44b4-bfe2-17b6293f0d89")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DiagonalTraverse/DiagonalTraverse/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiagonalTraverse
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var input = new int[][]
+                {
+                    new [] {1, 2, 3},
+                    new [] {4, 5, 6},
+                    new [] {7, 8, 9}
+                 };
+
+             var res = FindDiagonalOrder(input);
+        }
+
+        public int[] FindDiagonalOrder(int[][] matrix)
+        {
+
+            if (matrix.Length == 0) return new int[0];
+
+            var matrixLenght = matrix.Length * matrix[0].Length;
+            var res = new List<int>(){matrix[0][0]};
+            var vectorToUp = true;
+
+            int col = 0, line = 0;
+
+            while (matrixLenght != res.Count)
+            {
+                try
+                {
+                    var currCol = col;
+                    var currLine = line;
+
+                    if (vectorToUp)
+                    {
+                        res.Add(matrix[--currLine][++currCol]);
+                    }
+                    else
+                    {
+                        res.Add(matrix[++currLine][--currCol]); 
+                    }
+
+                    col = currCol;
+                    line = currLine;
+                }
+                catch (Exception ex)
+                {
+                    vectorToUp = !vectorToUp;
+
+                    var currCol = col;
+                    var currLine = line;
+
+                    if (vectorToUp)
+                    {
+                        try
+                        {
+                            res.Add(matrix[++currLine][currCol]); 
+                        }
+                        catch (Exception e)
+                        {
+                            res.Add(matrix[--currLine][++currCol]);
+                        }
+
+                    }
+                    else
+                    {
+                        try
+                        {
+                            res.Add(matrix[currLine][++currCol]); 
+                        }
+                        catch (Exception e)
+                        {
+                            res.Add(matrix[++currLine][--currCol]); 
+                        }
+                    }
+
+                    col = currCol;
+                    line = currLine;
+                }
+ 
+            }
+
+            return res.ToArray();
+        }
+    }
+}
