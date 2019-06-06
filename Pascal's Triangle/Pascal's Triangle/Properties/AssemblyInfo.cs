30a41b68718a80ac1b27e131ecbc35a64f51b28b
Maks <maxim.labutin1994@gmail.com>
2019-06-06 19:41:37 +0300

    Add Pascal's Triangle

---
+++ Pascal's Triangle/Pascal's Triangle/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pascal's Triangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pascal's Triangle")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00dc0445-d901-45e1-890c-cd1573f42e60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pascal's Triangle/Pascal's Triangle/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pascal_s_Triangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var res = Generate(2);
+        }
+
+
+        public IList<IList<int>> Generate(int numRows)
+        {
+
+            if (numRows == 0) return new List<IList<int>>();
+
+            IList<IList<int>> arrays = new List<IList<int>>()
+            {
+                new List<int>(1){1}
+            };
+
+            if (numRows == 1) return arrays;
+
+            arrays.Add(new List<int>(2) { 1, 1 });
+
+            if (numRows == 1) return arrays;
+
+
+            for (var level = 3; level <= numRows; level++)
+            {
+
+                var arr = GetNewArr(arrays[level - 2], level);
+                arrays.Add(arr);
+            }
+
+            return arrays;
+        }
+
+
+        //если уровень четный, то можно посчитать только половину еррея и потом развернуть результат и добавить в новый массивы
+        public List<int> GetNewArr(IList<int> prevList, int currLevel)
+        {
+            var list = new List<int>(currLevel) { 1, currLevel - 1 };
+              
+            for (var index = 2; index < currLevel - 1; index++)
+            {
+                list.Add(prevList[index - 1] + prevList[index]);
+            }
+
+            list.Add(1);
+
+            return list;
+        }
+    }
+}
