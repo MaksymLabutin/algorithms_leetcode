0b2c894f1dd8a09e0ff0d4e54ac005af39cbd0b7
Maks <maxim.labutin1994@gmail.com>
2020-05-17 16:40:00 +0300

    Fixed the solution

--- 665. Non-decreasing Array/665. Non-decreasing Array/UnitTest1.cs
+++ 665. Non-decreasing Array/665. Non-decreasing Array/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace Tests
         public void Test1()
         {
             var nums = new int[] { 4, 2, 3 };
-            var actual = CheckPossibility(nums); 
+            var actual = CheckPossibility(nums);
 
             Assert.True(actual);
         }
@@ -20,7 +20,7 @@ namespace Tests
         public void Test2()
         {
             var nums = new int[] { 4, 2, 1 };
-            var actual = CheckPossibility(nums); 
+            var actual = CheckPossibility(nums);
 
             Assert.False(actual);
         }
@@ -28,8 +28,8 @@ namespace Tests
         [Test]
         public void Test3()
         {
-            var nums = new int[] {3, 4, 2, 3};
-            var actual = CheckPossibility(nums); 
+            var nums = new int[] { 3, 4, 2, 3 };
+            var actual = CheckPossibility(nums);
 
             Assert.False(actual);
         }
@@ -37,8 +37,8 @@ namespace Tests
         [Test]
         public void Test4()
         {
-            var nums = new int[] {2, 3, 3, 2, 4};
-            var actual = CheckPossibility(nums); 
+            var nums = new int[] { 2, 3, 3, 2, 4 };
+            var actual = CheckPossibility(nums);
 
             Assert.True(actual);
         }
@@ -46,25 +46,28 @@ namespace Tests
         [Test]
         public void Test5()
         {
-            var nums = new int[] {-1, 4, 2, 3};
-            var actual = CheckPossibility(nums); 
+            var nums = new int[] { -1, 4, 2, 3 };
+            var actual = CheckPossibility(nums);
 
             Assert.True(actual);
         }
 
         public bool CheckPossibility(int[] nums)
-        { 
+        {
             var maxToModify = 1;
-
-            for (var i = nums.Length - 1; i > 0; i--)
+            var min = nums[nums.Length - 1];
+            for (var i = nums.Length - 1; i >= 0; i--)
             {
-                if (nums[i] >= nums[i - 1] && i + 1 < nums.Length && nums[i - 1] <= nums[i + 1]) continue;
+                min = Math.Min(min, nums[i]);
+                if (nums[i] <= min) continue;
                 maxToModify--;
-                nums[i] = i + 1 >= nums.Length ? int.MaxValue : nums[i + 1];
-                
+                if (maxToModify < 0 ) return false;
+                if (i + 2 < nums.Length && nums[i] > nums[i + 2]) min = Math.Min(min, nums[i]); 
+                else min = Math.Max(min, nums[i]);
             }
-             
-            return maxToModify >= 0;
-        }
+
+            return true;
+        } 
+         
     }
 }
