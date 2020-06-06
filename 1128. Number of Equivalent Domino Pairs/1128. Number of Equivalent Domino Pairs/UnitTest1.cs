d5790b462712453b3a2ecfca04fee0fa29209029
Maks <maxim.labutin1994@gmail.com>
2020-06-06 15:27:53 +0300

    Fixed 1128. Number of Equivalent Domino Pairs/1128. Number of Equivalent Domino Pairs

--- 1128. Number of Equivalent Domino Pairs/1128. Number of Equivalent Domino Pairs/UnitTest1.cs
+++ 1128. Number of Equivalent Domino Pairs/1128. Number of Equivalent Domino Pairs/UnitTest1.cs
@@ -13,19 +13,27 @@ namespace Tests
         //todo
         public int NumEquivDominoPairs(int[][] dominoes)
         {
-            var hs = new HashSet<(int, int)>();
+            var hs = new Dictionary<(int, int), int>();
             var res = 0;
             foreach (var domino in dominoes)
             {
                 if (domino[0] > domino[1])
                 {
-                    if(hs.Contains((domino[1], domino[0]))) res++;
-                    else hs.Add((domino[1], domino[0]));
+                    if (hs.ContainsKey((domino[1], domino[0])))
+                    {
+                        res += hs[(domino[1], domino[0])];
+                        hs[(domino[1], domino[0])] += 1;
+                    }
+                    else hs.Add((domino[1], domino[0]), 1);
                 }
                 else
                 {
-                    if (hs.Contains((domino[0], domino[1]))) res++;
-                    else hs.Add((domino[0], domino[1]));
+                    if (hs.ContainsKey((domino[0], domino[1])))
+                    {
+                        res += hs[(domino[0], domino[1])];
+                        hs[(domino[0], domino[1])] += 1;
+                    }
+                    else hs.Add((domino[0], domino[1]), 1);
                 }
             }
 
