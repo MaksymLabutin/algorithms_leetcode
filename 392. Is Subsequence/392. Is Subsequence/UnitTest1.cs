acf2498a5403495ba665ce784cf2e818208c4fbf
Maks <maxim.labutin1994@gmail.com>
2020-06-24 18:40:45 +0300

    Added 392. Is Subsequence solution

---
+++ 392. Is Subsequence/392. Is Subsequence/UnitTest1.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    { 
+        [Test]
+        public void Test1()
+        {
+            var s = "abc";
+            var t = "ahbgdc";
+
+            var actual = IsSubsequence(s, t); 
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void Test2()
+        { 
+            var s = "axc";
+            var t = "ahbgdc";
+
+            var actual = IsSubsequence(s, t); 
+            Assert.False(actual);
+        }
+
+        [Test]
+        public void Test3()
+        { 
+            var s = "";
+            var t = "ahbgdc";
+
+            var actual = IsSubsequence(s, t); 
+            Assert.True(actual);
+        }
+
+        [Test]
+        public void Test4()
+        { 
+            var s = "1";
+            var t = "";
+
+            var actual = IsSubsequence(s, t); 
+            Assert.False(actual);
+        }
+
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length > t.Length) return false;
+            if (s.Length == 0) return true;
+
+            var sPointer = 0;
+            foreach (var symbol in t)
+            {
+                if (s[sPointer] == symbol)
+                {
+                    sPointer++;
+                    if (sPointer > s.Length) return true;
+                }
+            }
+
+            return sPointer == s.Length;
+        }
+    }
+}
