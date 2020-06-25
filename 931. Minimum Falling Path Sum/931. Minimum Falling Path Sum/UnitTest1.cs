538c0cf36353e35e87607a7a3a611295c20a6706
Maks <maxim.labutin1994@gmail.com>
2020-06-25 21:52:24 +0300

    Added 931. Minimum Falling Path Sum solution

--- 931. Minimum Falling Path Sum/931. Minimum Falling Path Sum/UnitTest1.cs
+++ 931. Minimum Falling Path Sum/931. Minimum Falling Path Sum/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Tests
@@ -26,20 +27,65 @@ namespace Tests
             Assert.AreEqual(expected, actual);
         }
 
-        public int MinFallingPathSum(int[][] A)
+        [Test]
+        public void Test2()
         {
-
-            if (A.Length == 1) return 1;//todo
-
-            for (var i = A.Length - 1; i >= 0; i++)
+            var A = new int[][]
             {
-                for (var j = A[i].Length - 2; j < A[0].Length; j--)
+                new []{ 1, 2, 3 },
+                new []{ 4, 5, 6 }
+
+            };
+
+            var actual = MinFallingPathSum(A);
+
+            var expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var A = new int[][]
+            {
+                new []{ 1, 2, 3 }
+            };
+
+            var actual = MinFallingPathSum(A);
+
+            var expected = 1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        public int MinFallingPathSum(int[][] A)
+        { 
+            for (var row = A.Length - 2; row >= 0; row--)
+            {
+                for (var col = 0; col < A[row].Length; col++)
                 {
-                    if(i > 0 && j + 1 <= )
-                    A[]
-                }
+                    if (col > 0 && col + 1 < A[row].Length)
+                    {
+                        A[row][col] += Math.Min(A[row + 1][col - 1], Math.Min(A[row + 1][col], A[row + 1][col + 1]));
+                    }
+                    else if (col == 0)
+                    {
+                        A[row][col] += Math.Min(A[row + 1][col], A[row + 1][col + 1]);
+                    }
+                    else
+                    { 
+                        A[row][col] += Math.Min(A[row + 1][col], A[row + 1][col - 1]);
+                    }
+                } 
             }
-            return 1;
+            var min = int.MaxValue;
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                min = Math.Min(min, A[0][i]);
+
+            }
+
+            return min;
         }
     }
 }
