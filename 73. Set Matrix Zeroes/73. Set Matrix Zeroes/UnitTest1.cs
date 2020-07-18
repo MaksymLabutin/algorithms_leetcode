aaceaaf970adfc2a364ccf58e5fec62fedbb5aec
Maks <maxim.labutin1994@gmail.com>
2020-07-18 22:48:01 +0300

    Added 73. Set Matrix Zeroes solution

--- 73. Set Matrix Zeroes/73. Set Matrix Zeroes/UnitTest1.cs
+++ 73. Set Matrix Zeroes/73. Set Matrix Zeroes/UnitTest1.cs
@@ -97,8 +97,8 @@ namespace Tests
             var matrix = new int[][]
             {
                 new []{ 8,3,6,9,7,8,0,6 },
-                new []{0,3,7,0,0,4,3,8},
-                new []{ 5, 3, 6, 7, 1, 6, 2, 6 },
+                new []{ 0,3,7,0,0,4,3,8},
+                new []{ 5,3,6,7,1,6,2,6 },
                 new []{ 8,7,2,5,0,6,4,0},
                 new []{ 0,2,9,9,3,9,7,3}
             };
@@ -108,8 +108,8 @@ namespace Tests
             var expected = new int[][]
             {
                 new []{ 0,0,0,0,0,0,0,0 },
-                new []{0,0,0,0,0,0,0,0},
-                new []{ 0, 3, 6, 0, 0, 6, 0, 0 },
+                new []{ 0,0,0,0,0,0,0,0},
+                new []{ 0,3,6,0,0,6,0,0 },
                 new []{ 0,0,0,0,0,0,0,0 },
                 new []{ 0,0,0,0,0,0,0,0 }
             };
@@ -119,68 +119,66 @@ namespace Tests
 
         public void SetZeroes(int[][] matrix)
         {
-            var flag = int.MaxValue / 6;
+            var flag = false;
             for (var row = 0; row < matrix.GetLength(0); row++)
             {
-                for (var col = 0; col < matrix[row].Length; col++)
+                if (matrix[row][0] == 0) flag = true;
+
+                for (var col = 1; col < matrix[row].Length; col++)
                 {
                     if (matrix[row][col] != 0) continue;
 
-                    if (row == 0)
-                    {
-                        matrix[0][col] = -flag;
-                        matrix[row][0] = col == 0 || matrix[row][0] == 0 ? 0 : flag;
-                        continue;
-                    }
-
-                    matrix[row][0] = flag;
-                    if (matrix[0][col] != 0)
-                    {
-                        matrix[0][col] = -flag;
-                    }
+                    matrix[row][0] = 0;
+                    matrix[0][col] = 0;
 
                 }
             }
 
-            SetZeroesRows(matrix, flag);
-            SetZeroesCols(matrix, -flag);
-
-            //for (var row = matrix.GetLength(0) - 1; row >= 0; row--)
-            //{
-            //    for (var col = matrix[row].Length - 1; col >= 0; col--)
-            //    {
-            //        if (matrix[row][0] == 0 || matrix[row][0] == flag || matrix[0][col] == -flag || matrix[0][col] == 0) matrix[row][col] = 0;
-            //    }
-            //}
-        }
-
-        private void SetZeroesRows(int[][] matrix, int flag)
-        {
-            for (int row = 0; row < matrix.Length; row++)
+            for (var row = matrix.GetLength(0) - 1; row >= 0; row--)
             {
-                if (matrix[row][0] == 0 || matrix[row][0] == flag)
+                for (var col = matrix[row].Length - 1; col >= 1; col--)
                 {
-                    for (var col = 0; col < matrix[row].Length; col++) matrix[row][col] = 0;
+                    if (matrix[row][0] == 0 || matrix[0][col] == 0) matrix[row][col] = 0;
                 }
+
+                if (flag) matrix[row][0] = 0;
             }
         }
 
-        private void SetZeroesCols(int[][] matrix, int flag)
-        {
-            for (int row = 0; row < 1; row++)
-            {
-                for (int col = 0; col < matrix[row].Length; col++)
-                {
-                    if (matrix[row][col] == 0 || matrix[row][col] == flag)
-                    {
-                        for (int currRow = 0; currRow < matrix.Length; currRow++)
-                        {
-                            matrix[currRow][col] = 0;
-                        }
-                    }
-                }
-            }
-        }
+        //public void SetZeroes(int[][] matrix)
+        //{
+        //    var flag = int.MaxValue / 6;
+        //    for (var row = 0; row < matrix.GetLength(0); row++)
+        //    {
+        //        for (var col = 0; col < matrix[row].Length; col++)
+        //        {
+        //            if (matrix[row][col] != 0) continue;
+
+        //            if (row == 0)
+        //            {
+        //                matrix[0][col] = -flag;
+        //                matrix[row][0] = col == 0 || matrix[row][0] == 0 ? 0 : flag;
+        //                continue;
+        //            }
+
+        //            matrix[row][0] = flag;
+        //            if (matrix[0][col] != 0)
+        //            {
+        //                matrix[0][col] = matrix[0][col] == flag ? 0 :  - flag;
+        //            }
+
+        //        }
+        //    }
+
+        //    for (var row = matrix.GetLength(0) - 1; row >= 0; row--)
+        //    {
+        //        for (var col = matrix[row].Length - 1; col >= 0; col--)
+        //        {
+        //            if (matrix[row][0] == 0 || matrix[row][0] == flag || matrix[0][col] == -flag || matrix[0][col] == 0) matrix[row][col] = 0;
+        //        }
+        //    }
+        //}
+
 
     }
 }
